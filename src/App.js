0d67aed0331ff382f84cf47d2d077b1ee28665ab
Julio <julio.python777@gmail.com>
2022-05-05 00:05:19 -0300

    fazendo testes iniciais

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
+import { Container,Menu,PageBody } from './AppStyled';
+
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
 
@@ -10,16 +12,25 @@ export default () => {
 
     return (
         <BrowserRouter>
+        <Container>
+            
+            <Menu>
+
+            </Menu>
+            <PageBody>
+                <Switch>
+                    <Route exact path="/">
+                        <HomeScreen />
+                    </Route>
+                    <Route path="/tela2/:nome">
+                        <Tela2Screen />
+                    </Route>
+                </Switch>
+            </PageBody>
+        </Container>
             <h1>Oi, {name}</h1>
 
-            <Switch>
-                <Route exact path="/">
-                    <HomeScreen />
-                </Route>
-                <Route path="/tela2/:nome">
-                    <Tela2Screen />
-                </Route>
-            </Switch>
+            
         </BrowserRouter>
     );
 }
---
+++ src/AppStyled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display:flex ;
+    height: 100vh ;
+    background-color:aqua ;
+`;
+
+export const Menu = styled.div`
+    display:flex ;
+    background-color:grey ;
+    width:80px ;
+`;
+
+export const PageBody = styled.div`
+    display:flex ;
+    background-color:red;
+`;
